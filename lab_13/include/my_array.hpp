7c2afc211ffdd418f6a29c57a77c2882e6c60fd4
Mark Prudnikov <mark.prudnikov@inbox.ru>
2021-04-17 17:52:21 +0300

    Boolean type

--- lab_13/include/my_array.hpp
+++ lab_13/include/my_array.hpp
@@ -1,28 +1,28 @@
 #include <cassert>
 
 #pragma once
-#include <bitset>
+
 namespace lab_13 {
 
     template<typename T, std::size_t N>
     class my_array {
     public:
 
-        T& operator[](std::size_t index) {
+        T &operator[](std::size_t index) {
             return _data[index];
         }
 
-        T const& operator[](std::size_t index) const {
+        T const &operator[](std::size_t index) const {
             return _data[index];
         }
 
-        T& at(std::size_t index) {
+        T &at(std::size_t index) {
             assert(index < N);
 
             return _data[index];
         }
 
-        T const& at(std::size_t index) const {
+        T const &at(std::size_t index) const {
             assert(index < N);
 
             return _data[index];
@@ -42,38 +42,86 @@ namespace lab_13 {
         }
 
     private:
-        T _data[N]{};
+        T _data[N];
     };
 
     template<std::size_t N>
     class my_array<bool, N> {
+    private:
+        class bool_ref;
+
     public:
-        class bool_ref {
-        public:
-            bool_ref() = default;
-
-            explicit operator bool() const {
-                //TODO("")
-            }
-
-            bool operator=(bool new_value) {
-                //TODO("")
-            }
-
-        private:
-
-        };
-
         bool operator[](std::size_t index) const {
-            //TODO("");
+            return bool_ref(_data, index);
         }
 
         bool_ref operator[](std::size_t index) {
-            return bool_ref();
+            return bool_ref(&_data[0], index);
+        }
+
+        bool empty() const {
+            return false;
+        }
+
+        std::size_t size() const {
+            return N;
+        }
+
+        void fill(bool val) {
+            for (std::size_t index = 0; index < N; ++index)
+                bool_ref(&_data[0], index) = val;
+        }
+
+        bool_ref at(std::size_t index) {
+            assert(index < N);
+
+            return bool_ref(&_data[0], index);
+        }
+
+        bool at(std::size_t index) const {
+            assert(index < N);
+
+            return bool_ref(&_data[0], index);
         }
 
     private:
-        uint8_t _data[!((N - 1) & N && N >= 8) ? N / 8 : N / 8 + 1];
+        static const uint8_t _base = 8;
+        static const uint8_t _size = !(N % _base) ? N / _base : N / _base + 1;
+        uint8_t _data[_size];
+
+        class bool_ref {
+        private:
+            friend class my_array;
+            bool_ref(uint8_t *data, std::size_t index) :
+                    data(data),
+                    mask(1 << (index % _base)),
+                    data_index(index / _base)
+            {};
+        public:
+
+            operator bool() const {
+                return (bool) (data[data_index] & mask);
+            }
+
+            bool_ref &operator=(bool new_value) {
+                new_value ? data[data_index] |= mask : data[data_index] &= ~mask;
+                return *this;
+            }
+
+            bool_ref &operator=(bool_ref const &new_value) {
+                if (this == &new_value)
+                    return *this;
+
+                *this = (bool) new_value;
+                return *this;
+            }
+
+        private:
+            uint8_t *data;
+            uint8_t mask;
+            std::size_t data_index;
+        };
+
     };
 
 }
--- lab_13/src/main.cpp
+++ lab_13/src/main.cpp
@@ -2,8 +2,25 @@
 
 #include "my_array.hpp"
 
+template<typename T, std::size_t N>
+void test_core() {
+    lab_13::my_array<T, N> a;
+    const lab_13::my_array<T, N> &ca = a;
+    a.empty();
+    ca.empty();
+    a.size();
+    ca.size();
+
+    a.at(0);
+    ca.at(0);
+    a[0];
+    ca[0];
+    (void)static_cast<const T &>(a[0]);
+    (void)static_cast<const T &>(ca[0]);
+}
+
 int main() {
-    lab_13::my_array<int, 20> not_empty{};
+    lab_13::my_array<int, 20> not_empty;
 
     not_empty.fill(1);
 
@@ -12,6 +29,23 @@ int main() {
     std::cout << not_empty.empty() << std::endl;
 
     std::cout << not_empty.at(4) << std::endl;
+
+    lab_13::my_array<bool, 35> bitset;
+
+    bitset.fill(true);
+    assert(bitset[1] == true);
+    assert(bitset[0] == true);
+
+    bitset[0] = false;
+    assert(bitset[0] == false);
+
+    bitset[0] = bitset[1];
+    assert(bitset[0] == bitset[1]);
+    assert(bitset[0] == true);
+    assert(bitset.at(1) ==  true);
+
+    //test_core<int, 10>();
+    test_core<bool, 10>();
 
     return 0;
 }
